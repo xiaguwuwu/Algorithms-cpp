da890ee869b77f6f112ec9237702b9859b77880a
Perfect <534963019@qq.com>
2025-03-10 23:22:16 +0800

    Update and rename 字符串插入 to 字符串插入.cpp

---
+++ String/字符串插入.cpp
@@ -0,0 +1,33 @@
+//题目描述：  
+//给定两个字符串A和B，请判断是否可以在字符串A的某个位置插入一个字符串B，使得插入后的新字符串成为一个回文串。回文串是指正读和反读都相同的字符串。
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+
+    int a_length = a.length();
+    for(int i =0;i<a.length();i++){
+        string temp;
+        temp = a.substr(0,i)+ b + a.substr(i);
+        
+        int n = 0,m = temp.length()-1;
+        while(n <= m){
+            if(temp[n] != temp[m]){
+                break;
+            }
+            n++;
+            m--;
+        }
+        if(n >= m){
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
